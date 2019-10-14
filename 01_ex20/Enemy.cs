d60541e88cd54fee3e50cb596bfba42c5decbdae
PmaiWoW <PmaiWoW@Gmail.com>
2019-10-14 21:24:27 +0100

    Add resolution of ex20 of 01

---
+++ 01_ex20/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_ex20
+{
+    public class Enemy
+    {
+        public static int NumberOfEnemies { get; private set; }
+        public int Health { get; set; }
+
+        public Enemy(int health)
+        {
+            NumberOfEnemies++;
+            Health = health;
+        }
+
+        public void Die()
+        {
+            NumberOfEnemies--;
+            Health = 0;
+        }
+    }
+}
---
+++ 01_ex20/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01_ex20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy monster = new Enemy(100);
+
+            Console.WriteLine($"{monster.Health}");
+            Console.WriteLine($"{Enemy.NumberOfEnemies}");
+        }
+    }
+}
